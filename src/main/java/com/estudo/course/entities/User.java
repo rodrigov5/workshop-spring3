1403ae9d558ee721ee78cf1c39b273733d776e9f
Rodrigo Veloso <57206601+rodrigov5@users.noreply.github.com>
2023-12-04 00:28:49 -0300

    :hammer: Order, Instant, ISO 8601 parte1 :hammer:

--- src/main/java/com/estudo/course/entities/User.java
+++ src/main/java/com/estudo/course/entities/User.java
@@ -1,11 +1,10 @@
 package com.estudo.course.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -20,6 +19,13 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
+    public List<Order> getOrders() {
+        return orders;
+    }
 
     public User() {}
 
